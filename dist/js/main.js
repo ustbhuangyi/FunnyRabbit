a7df7e7d5f4e004b6540896e0b180bde8e98053f
ustbhuangyi <280309453@qq.com>
2015-04-01 17:40:51 +0800

    m hao123cake

--- dist/js/main.js
+++ dist/js/main.js
@@ -207,6 +207,54 @@ for (var i in res) {
   g_resources.push(res[i]);
 }
 
+var GamePlayScene  = cc.Scene.extend({
+  onEnter:function () {
+    this._super();
+
+    var layer = new GamePlayLayer();
+    this.addChild(layer);
+
+  }
+});
+
+var GamePlayLayer = cc.Layer.extend({
+
+  backgroundLayer : null,
+  touchLayer : null,
+  ctor : function(){
+    this._super();
+
+    this.addCache();
+
+    this.addBackgroundLayer();
+
+    this.addTouchLayer();
+  },
+
+  addCache : function(){
+
+    //将plist添加到缓存
+    cc.spriteFrameCache.addSpriteFrames(res.rabbit_small_plist);
+    cc.spriteFrameCache.addSpriteFrames(res.rabbit_big_plist);
+    cc.spriteFrameCache.addSpriteFrames(res.rabbit_win_plist);
+    cc.spriteFrameCache.addSpriteFrames(res.rabbit_lose_plist);
+    cc.spriteFrameCache.addSpriteFrames(res.icons_plist);
+    cc.spriteFrameCache.addSpriteFrames(res.explosion_plist);
+  },
+
+
+  addBackgroundLayer : function(){
+
+    this.backgroundLayer = new GPBackgroundLayer();
+    this.addChild(this.backgroundLayer);
+  },
+
+  addTouchLayer : function(){
+    this.touchLayer = new GPTouchLayer();
+    this.addChild(this.touchLayer);
+  }
+});
+
 var GPBackgroundLayer = cc.LayerColor.extend({
 
   ctor: function (color) {
@@ -1292,53 +1340,5 @@ var TimeSprite = cc.Sprite.extend({
     this.value = value;
     var frame = this.getTimeFrame();
     this.setSpriteFrame(frame);
-  }
-});
-
-var GamePlayScene  = cc.Scene.extend({
-  onEnter:function () {
-    this._super();
-
-    var layer = new GamePlayLayer();
-    this.addChild(layer);
-
-  }
-});
-
-var GamePlayLayer = cc.Layer.extend({
-
-  backgroundLayer : null,
-  touchLayer : null,
-  ctor : function(){
-    this._super();
-
-    this.addCache();
-
-    this.addBackgroundLayer();
-
-    this.addTouchLayer();
-  },
-
-  addCache : function(){
-
-    //将plist添加到缓存
-    cc.spriteFrameCache.addSpriteFrames(res.rabbit_small_plist);
-    cc.spriteFrameCache.addSpriteFrames(res.rabbit_big_plist);
-    cc.spriteFrameCache.addSpriteFrames(res.rabbit_win_plist);
-    cc.spriteFrameCache.addSpriteFrames(res.rabbit_lose_plist);
-    cc.spriteFrameCache.addSpriteFrames(res.icons_plist);
-    cc.spriteFrameCache.addSpriteFrames(res.explosion_plist);
-  },
-
-
-  addBackgroundLayer : function(){
-
-    this.backgroundLayer = new GPBackgroundLayer();
-    this.addChild(this.backgroundLayer);
-  },
-
-  addTouchLayer : function(){
-    this.touchLayer = new GPTouchLayer();
-    this.addChild(this.touchLayer);
   }
 });
